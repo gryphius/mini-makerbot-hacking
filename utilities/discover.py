dae24e332e5f095e5be4c2bf00adf741fb7f1374
gryphius <oli@fuglu.org>
2014-09-07 20:34:35 +0200

    timeout handling and (hopefully) support for multiple printers

--- utilities/discover.py
+++ utilities/discover.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
 import socket
 import json
+import sys
+import time
+
 host='255.255.255.255'
 
 target_port = 12307
@@ -14,21 +17,34 @@ broadcastsocket.bind(('0.0.0.0', source_port))
 answersocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 discover_request='{"command": "broadcast"}'
 answersocket.bind(('0.0.0.0', listen_port))
+answersocket.settimeout(3)
 
+answers=[]
+knownbotips=[]
 print "Searching for a makerbot..."
-broadcastsocket.sendto(discover_request, (host,target_port),)
-answersocket.settimeout(10)
-try:
-    data, fromaddr = answersocket.recvfrom(1024)
-    print "Got a reply from ",fromaddr
-except:
-    print "No bot found :("
+for _ in range(3):
+    broadcastsocket.sendto(discover_request, (host,target_port),)
+    try:
+        data, fromaddr = answersocket.recvfrom(1024)
+        if fromaddr not in knownbotips:
+            knownbotips.append(fromaddr)
+            print "Got a reply from ",fromaddr
+            answers.append(data)
+        else:
+            continue
+    except:
+        continue
+    time.sleep(1)
 
-infodic=json.loads(data)
+if len(answers)==0:
+    print "No bots found :("
+    sys.exit(1)
 
-print ""
-print "Machine Name: ",infodic['machine_name']
-print "IP Address: ",infodic['ip']
-print "Serial: ",infodic['iserial']
+for data in answers:
+    infodic=json.loads(data)
+    print ""
+    print "Machine Name: ",infodic['machine_name']
+    print "IP Address: ",infodic['ip']
+    print "Serial: ",infodic['iserial']
 
 
